3c8e59bb0238c5a105282ef26e1926fbae273bae
Katie McLaughlin <kt.mclaughlin16@gmail.com>
2020-08-26 17:54:54 -0700

    compiles and runs, fixed syntax

--- TransitCalc.cpp
+++ TransitCalc.cpp
@@ -1,20 +1,24 @@
-// TransitCalc.cpp
+/*
+* TransitCalc.cpp
+*/
 
 #include <iostream>
-#include "math.h"
+#include <iomanip>
+#include <algorithm>
 #include "TransitCalc.h"
+#include "math.h"
 using namespace std;
 
-TransitCalc::TransitCalc(int singleDays, int numRides, bool discount){
-    double PRICES[3] = {2.75, 33.0, 127};
-    double REDUCED[3] = {1.35, 16.50, 63.50};
-    const double DAYS[3] = {1, 7, 30};
+double PRICES[3] = {2.75, 33.0, 127};     // regular prices
+double REDUCED[3] = {1.35, 16.50, 63.50}; // reduced prices
+const double DAYS[3] = {1, 7, 30};        // number of days per pass option
 
+TransitCalc::TransitCalc(int singleDays, int numRides, bool discount){
     days = singleDays;
     rides = numRides;
 
     if(discount) {
-        PRICES[3] = REDUCED[3];
+        memcpy(PRICES, REDUCED, sizeof(PRICES));
     }
 }
 
@@ -34,35 +38,53 @@ double* TransitCalc::getRidePrices(){
     pricePer[0] = PRICES[0]; // single pay per ride
     pricePer[1] = unlimited7Price(); // per ride w/ 7 day pass
         
-    double numTix = ceil(days / DAYS[2]); // hround up to full 30 day 
+    double numTix = ceil(days / DAYS[2]); // round up to full 30 day 
     double totalPrice = numTix * PRICES[2]; // total price of all tix
 
     // round to nearest cent
     double priceEach = round((totalPrice / (double) rides) * 100) / 100.0; 
     pricePer[2] = priceEach;
-
-    printPricePer(pricePer);
         
     return pricePer;
 }
 
-void TransitCalc::printPricePer(double* array){
-    for(int c = 0; c < sizeof(array); c++) {
-        std::cout << array[c] << " " << std::endl;
-    }
-}
-
 void TransitCalc::getBestFare(){
     double* costs = getRidePrices();
-    double min = std::min((std::min(costs[0], costs[1])), costs[2]);
-    std::string type[3]= {"Pay-per-ride ", "7-day Unlimited ", "30-day Unlimited "};
+    double minVal = min((min(costs[0], costs[1])), costs[2]);
+    string pass[3]= {"Pay-per-ride", "7-day Unlimited", "30-day Unlimited"};
     int ind = 2;
-    if(min == costs[1]) {
+    if(minVal == costs[1]) {
         ind = 1;
-    } if(min == costs[0]) {
+    } if(minVal == costs[0]) {
         ind = 0;
     }
     double cost = costs[ind];
 
-    cout << "You should get the " << type[ind] << " option at $" << costs[ind] << " per ride." << endl;
+    cout << "You should get the " << pass[ind] << " option at $" << costs[ind] << " per ride." << endl;
+}
+
+int main() {
+
+    int days = 2;
+    int rides = 5;
+    bool disc = true;
+
+    TransitCalc* test = new TransitCalc(days, rides, disc);
+
+    double* prices = test->getRidePrices();
+
+    // Print 1 day
+    cout << "Price per ride with Pay-per-ride: $" << prices[0] << endl;
+
+    // Print 7 day 
+    cout << "Price per ride with 7-day Unlimited: $";
+    cout << fixed;
+    cout << setprecision(2);
+    cout << test->unlimited7Price() << endl;
+
+    // Print 30 day
+    cout << "Price per ride with 30-day Unlimited: $" << prices[2] << endl;
+
+    // Recommendation
+    test->getBestFare();
 }
--- TransitCalc.h
+++ TransitCalc.h
@@ -1,5 +1,6 @@
 // TransitCalc.h
 
+// #include <iostream>
 #pragma once
 
 #ifndef TRANSIT_CALC_H
@@ -7,31 +8,27 @@
 
 namespace std{
     class TransitCalc {
+    
     public: 
+        // intialize variables for number days, rides, and if eligible for discount
         TransitCalc(int singleDays, int numRides, bool discount);
         
+        // returns price per ride with 7 day unlimited option
         double unlimited7Price();
         
+        // returns double array of the price/ride per each fare option
         double* getRidePrices();
         
-        void printPricePer(double* array);
-        
+        // prints recommendation ticket type and price
         void getBestFare();
-
+    
     private:
-        int days;
-        
-        int rides;
-        
-        static double PRICES[3];
-        
-        static double REDUCED[3];
-        
-        static double DAYS[3];
+        int days; // number of days using transit
+
+        int rides; // number of rides using transit
+
 };
 }
-
-
 
 
 #endif /* TRANSIT_CALC_H */
